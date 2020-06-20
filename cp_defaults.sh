2540c2007796358c03113f590c3b163f82a8bbfc
Piasy <xz4215@gmail.com>
2020-06-20 12:36:04 +0800

    local test

---
+++ cp_defaults.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp source/data_access/defaults.js dist/portal/data_access/defaults.js
+cp source/data_access/defaults.js dist/management_api/data_access/defaults.js
+cp source/data_access/defaults.js dist/conference_agent/data_access/defaults.js
+cp source/data_access/defaults.js dist/sip_portal/data_access/defaults.js
+
